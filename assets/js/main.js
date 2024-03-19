7adbac883c8e3ec22045299e888993a24424b5cb
antden99 <antoniodenigris@icloud.com>
2024-03-19 14:05:07 +0100

    add function allApi and intern loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,15 @@ createApp({
     },
     methods: {
         callApi() {
-            axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    console.log(response.data.response);
-                    this.emailList.push(response.data.response)
-                })
+            for (let i = 0; i < 10; i++) {
+
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        console.log(response.data.response);
+                        this.emailList.push(response.data.response)
+                    })
+            }
         }
     },
     mounted() {
